1df07754a6b01e9f55f3cfac901af48e2ee78a67
MirSiam10 <mir10siam@gmail.com>
2026-05-31 01:32:49 +0600

    listsfuncs

--- Chapter_3(Lists&Tuples)/lists.py
+++ Chapter_3(Lists&Tuples)/lists.py
@@ -11,3 +11,16 @@ print(friends.index("Mir")) # 0
 print(len(friends)) # 4
 print(friends.append("Sifat")) # None
 print(friends) # ['Mir', 'Siam', 'Rafi', 'Shawon', 'Sifat']
+print(friends.insert(2, "Sifat")) # None
+print(friends) # ['Mir', 'Siam', 'Sifat', 'Rafi', 'Shawon', 'Sifat']
+print(friends.remove("Sifat")) # None
+print(friends) # ['Mir', 'Siam', 'Rafi', 'Shawon', 'Sifat']
+print(friends.pop()) # Sifat
+print(friends) # ['Mir', 'Siam', 'Rafi', 'Shawon']
+print(friends.sort()) # None
+print(friends) # ['Mir', 'Rafi', 'Shawon', 'Siam']
+print(friends.reverse()) # None
+print(friends) # ['Siam', 'Shawon', 'Rafi', 'Mir']
+print(friends.clear()) # None
+print(friends) # []
+
