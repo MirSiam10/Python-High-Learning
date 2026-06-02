972766475751db8773ec31f1181d010a5c6524d2
MirSiam10 <mir10siam@gmail.com>
2026-06-02 14:53:51 +0600

    Marks Published

---
+++ Python_OOPs/students_marks.py
@@ -0,0 +1,40 @@
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+
+        if 0 <= grade <= 100:
+         self.grade = grade
+        else:
+           print("Grade must be in 0 to 100")
+           self.grade = 0
+
+    def get_letter_grade(self):
+       
+        if self.grade >= 90 :
+           print(f"{self.name} got A")
+        elif self.grade >= 80:
+           print(f"{self.name} got B")
+        elif self.grade >= 70:
+           print(f"{self.name} got C")
+        elif self.grade >= 60:
+           print(f"{self.name} got D")
+        elif self.grade >= 50:
+           print(f"{self.name} got F")
+        else:
+           print("No Result")
+
+    def is_passing(self):
+       if self.grade >= 60:
+          return True
+       else:
+          return False
+
+ashin = Student("Ashin", 90)
+arsalan = Student("Arsalan", 80)
+afsana = Student("Afsana", 45)
+ashin.get_letter_grade()
+print(f"{ashin.name} is passing: {ashin.is_passing()}")
+arsalan.get_letter_grade()
+print(f"{arsalan.name} is passing: {arsalan.is_passing()}")
+afsana.get_letter_grade()
+print(f"{afsana.name} is passing: {afsana.is_passing()}")
