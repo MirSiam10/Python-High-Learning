91763eadf67cdfbbd9b0d33751f0ce46ff28b914
MirSiam10 <mir10siam@gmail.com>
2026-06-02 15:23:14 +0600

    rebase

--- Python_OOPs/P1_(Class&Objects)/students_marks.py
+++ Python_OOPs/P1_(Class&Objects)/students_marks.py
@@ -7,7 +7,7 @@ class Student:
         else:
            print("Grade must be in 0 to 100")
            self.grade = 0
-
+#Method to get letter grade
     def get_letter_grade(self):
        
         if self.grade >= 90 :
@@ -18,20 +18,23 @@ class Student:
            print(f"{self.name} got C")
         elif self.grade >= 60:
            print(f"{self.name} got D")
-        elif self.grade >= 50:
-           print(f"{self.name} got F")
+        # elif self.grade >= 50:
+        #    print(f"{self.name} got F")
         else:
-           print("No Result")
-
+           print(f"{self.name} Failed")
+#Method to check if the student is passing
     def is_passing(self):
        if self.grade >= 60:
           return True
        else:
           return False
-
+       
+#Creating Student Objects
 ashin = Student("Ashin", 90)
 arsalan = Student("Arsalan", 80)
 afsana = Student("Afsana", 45)
+
+#Methods Called
 ashin.get_letter_grade()
 print(f"{ashin.name} is passing: {ashin.is_passing()}")
 arsalan.get_letter_grade()
